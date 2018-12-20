65b5ab6c1e90ff576e7f12ae9017afa61d3fb69b
Cool Lime <mz.zhoumeng@gmail.com>
2018-12-19 19:47:37 -0800

    Merge Sort Updates

--- src/A/SortingAlgorithms/Medium/MergeSort/MergeSort.java
+++ src/A/SortingAlgorithms/Medium/MergeSort/MergeSort.java
@@ -31,23 +31,24 @@ public class MergeSort {
     }
 
     private void sortArray(int[] array, int[] temp, int start, int end) {
-        // Base case: when the subarray cannot be divided in half anymore
+        // Base case: when the array cannot be divided in half any more
         if (start >= end) {
             return;
         }
-        // Divide the array in half, sort the two subarrays respectively
+        // Divide the array into two halves each time
+        // And sort the two parts respectively
         int mid = start + (end - start) / 2;
         sortArray(array, temp, start, mid);
         sortArray(array, temp, mid + 1, end);
-        // Merge the two subarrays into one sorted array
+        // Merge the two sorted halves into one big sorted array
         mergeArray(array, temp, start, mid, end);
     }
 
     private void mergeArray(int[] array, int[] temp, int start, int mid, int end) {
-        // Copy the array to temp such that we can do comparisons on the temp array
-        // while operating the original array --> make it look like in-place
+        // Copy the array to temp such that we can do comparisons only on the temp array,
+        // while operating on the original array to make it like in-place operations
         temp = Arrays.copyOf(array, array.length);
-        // Merge the two halves
+        // Merge the two parts according to their values: smaller --> larger
         int left = start;
         int right = mid + 1;
         int index = start;
@@ -58,7 +59,8 @@ public class MergeSort {
                 array[index++] = temp[right++];
             }
         }
-        // Only the left side is possible to be left with unsorted elements because the right hand side will always be greater
+        // When there are left-over elements, only the ones in the left part should be concerned
+        // because the ones in the right part will always be greater.
         while (left <= mid) {
             array[index++] = temp[left++];
         }
