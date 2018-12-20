d12d711ab2eb3ab1ab1fdf84c826d4c9caa02c98
Cool Lime <mz.zhoumeng@gmail.com>
2018-12-19 19:33:06 -0800

    Quick Sort

---
+++ src/A/SortingAlgorithms/Medium/QuickSort/QuickSort.java
@@ -0,0 +1,21 @@
+package A.SortingAlgorithms.Medium.QuickSort;
+
+/**
+ * https://app.laicode.io/app/problem/10
+ * Description
+ * Given an array of integers, sort the elements in the array in ascending order. The quick sort algorithm should be used to solve this problem.
+ *
+ * Examples
+ *
+ * {1} is sorted to {1}
+ * {1, 2, 3} is sorted to {1, 2, 3}
+ * {3, 2, 1} is sorted to {1, 2, 3}
+ * {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+ * Corner Cases
+ *
+ * What if the given array is null? In this case, we do not need to do anything.
+ * What if the given array is of length zero? In this case, we do not need to do anything.
+ **/
+
+public class QuickSort {
+}
